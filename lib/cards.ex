e89e2c2dc5a1a09149d08bd2644b22b15e236030
wael-Fadlallah <wael.fudlallah@gmail.com>
2021-03-12 17:39:29 +0400

    add docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,10 @@ defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
   """
+
+  @doc """
+    create a new deck
+  """
   def create_deck do
     values = ["Ace", "two", "Three", "Four", "Five"]
     suits  = ["Spades", "Clubs", "Hearts", "Dimonds"]
@@ -12,23 +16,42 @@ defmodule Cards do
 
   end
 
+  @doc """
+    shuffle a deck
+
+    prams
+
+    `deck` the deck that we want to shuffle
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    check if a deck contain a card
+  """
   def contains?(deck,card) do
     Enum.member?(deck,card)
   end
 
+  @doc """
+    deal from a deck
+  """
   def deal(deck,hand_size) do
     Enum.split(deck,hand_size)
   end
 
+  @doc """
+    save a deck to a file
+  """
   def save(deck,filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename,binary)
   end
 
+  @doc """
+    load a deck from a file
+  """
   def load() do
     case File.read("files") do
       {:ok,binary} -> :erlang.binary_to_term(binary)
